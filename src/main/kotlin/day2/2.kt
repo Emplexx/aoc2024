ef3fc84e20329ac56582b3001a5412ac22be97be
Emplexx <ishchuk132@gmail.com>
2024-12-02 14:28:44 +0100

    Day 2 Part 2 (Brute force)

--- src/main/kotlin/day2/2.kt
+++ src/main/kotlin/day2/2.kt
@@ -6,11 +6,13 @@ import kotlin.math.abs
 typealias Report = List<Int>
 typealias Input = List<Report>
 
-fun main() = runDay(2, ::partOne) { input ->
+fun main() = runDay(2, ::partOne, ::partTwo) { input ->
     input.lines().map { it.split(" ").map(String::toInt) }
 }
 
-fun partOne(input: Input) = input.count { it.isSameDirection() && it.isWithinSafeDifference() }
+fun partOne(input: Input) = input.count { it.isSafe() }
+
+fun Report.isSafe() = isSameDirection() && isWithinSafeDifference()
 
 fun Report.isSameDirection() = zipWithNext().run {
     all { (a, b) -> a > b } || all { (a, b) -> a < b }
@@ -19,7 +21,10 @@ fun Report.isSameDirection() = zipWithNext().run {
 fun Report.isWithinSafeDifference() =
     zipWithNext().all { (a, b) -> abs(a - b) in 1..3 }
 
-fun partTwo(input: Input) {
-    val (safe, other) = input.partition { it.isSameDirection() && it.isWithinSafeDifference() }
-    TODO()
-}
+fun partTwo(input: Input) = input.count { it.isSafe() || it.bruteForceSafety() }
+
+fun Report.bruteForceSafety(): Boolean =
+    foldIndexed(false) { i, acc, _ ->
+        if (acc) return true
+        toMutableList().apply { removeAt(i) }.isSafe()
+    }
